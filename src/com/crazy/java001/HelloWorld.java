5b0337f66475fbc6d8b1fb6429053af9caa4e88a
1134005157 <1134005157@qq.com>
2020-06-02 16:51:22 +0800

    hello java

---
+++ src/com/crazy/java001/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.crazy.java001;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+    }
+}
