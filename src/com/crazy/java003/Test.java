03f417f610e7835c6f13a39f698716194bfb2920
liuxinjie123 <jackdream@163.com>
2020-06-16 23:41:32 +0800

    object oriented

---
+++ src/com/crazy/java003/Test.java
@@ -0,0 +1,25 @@
+package com.crazy.java003;
+
+/**
+ * @Description
+ * <br /> Copyright (C), 2020-2030, jack
+ * <br /> This program is protected by copyright laws.
+ * <br /> Program Name: Test
+ * @Date 2020年6月16日
+ * @author jack
+ * @version 1.0
+ */
+public class Test {
+    /**
+     * name
+     */
+    public String name;
+
+    /**
+     * mobile phone num
+     */
+    public String mobile;
+
+    public Test() {}
+
+}
---
+++ src/com/crazy/java003/TestJavadoc.java
@@ -0,0 +1,16 @@
+package com.crazy.java003;
+
+/**
+ * @Description
+ * <br />  Copyright (C) 2020-2030 jack
+ * <br /> This program is protected by copyright laws.
+ * <br />Program Name: Test Javadoc
+ * @Date 2020年6月16日
+ * @author jack
+ * @version 1.0
+ */
+public class TestJavadoc {
+    public static void main(String[] args) {
+        System.out.println("Hello Javadoc!");
+    }
+}
